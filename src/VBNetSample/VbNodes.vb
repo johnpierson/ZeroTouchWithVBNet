979740459757f25adfe69b3a3514ccca285a5092
john pierson <sixtysecondrevit@gmail.com>
2021-04-15 09:45:47 -0600

    Initial Commit

---
+++ src/VBNetSample/VbNodes.vb
@@ -0,0 +1,25 @@
+﻿Imports Autodesk.DesignScript.Runtime
+Imports Point = Autodesk.DesignScript.Geometry.Point
+
+
+Public Module VbNodes
+    Public Function CreatePoint(Optional x As Double = 0, Optional y As Double = 0, Optional z As Double = 0)
+        ' Return
+        Dim newPoint As Point = Nothing
+        newPoint = Point.ByCoordinates(x,y,z)
+        Return newPoint
+    End Function
+
+    <MultiReturn({"x", "y", "z"})>
+    Public Function DesconstructPoint(point As Point)
+        ' Step 1: create a Dictionary.
+        Dim dictionary As New Dictionary(Of String, Double)
+
+        dictionary.Add("x",point.X)
+        dictionary.Add("y",point.Y)
+        dictionary.Add("z",point.Z)
+
+        Return dictionary
+    End Function
+
+End Module
